23a1d2c26d5b71899b7e6c1569c395002c5e3565
Xin Fan <2186465387@qq.com>
2025-02-23 02:32:04 +0800

    EntryPoint

---
+++ Hazel/src/Hazel/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// This is a macro that will be defined in the project settings
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifdef HZ_BUILD_DLL
+		#define HAZEL_API __declspec(dllexport)
+	#else
+		#define HAZEL_API __declspec(dllimport)
+	#endif // HZ_BUILD_DLL
+#else
+	#error Hazel only supports Windows!
+#endif // HZ_PLATFORM_WINDOWS
