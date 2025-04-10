74548e6a28b261f87655414227c0e59875e6ea91
Xin Fan <2186465387@qq.com>
2025-04-11 00:02:39 +0800

    premake

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,7 +2,7 @@
 
 // For use by Hazel applications
 
-#include "Hazel\Application.h"
+#include "Hazel/Application.h"
 #include "Hazel/Log.h"
 // -------EntryPoint----------------
 #include "Hazel/EntryPoint.h"
