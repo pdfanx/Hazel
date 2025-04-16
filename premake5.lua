ac3377142e254daa27f96e7d95af4e06adc70361
Xin Fan <2186465387@qq.com>
2025-04-16 23:52:44 +0800

    窗口抽象与GLFW Window Abstraction and GLFW

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,12 @@ workspace "Hazel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+
+include "Hazel/vendor/GLFW"
+
 project "Hazel"
 	location "Hazel"
 	kind "SharedLib"
@@ -30,7 +36,14 @@ project "Hazel"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
 	}
 
 	filter "system:windows"
@@ -51,14 +64,17 @@ project "Hazel"
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
+		buildoptions "/utf-8"
 		symbols "ON"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
+		buildoptions "/utf-8"
 		optimize "ON"
 
 	filter "configurations:DIST"
 		defines "HZ_DIST"
+		buildoptions "/utf-8"
 		optimize "ON"
 
 
@@ -99,12 +115,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
+		buildoptions "/utf-8"
 		symbols "ON"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
+		buildoptions "/utf-8"
 		optimize "ON"
 
 	filter "configurations:DIST"
 		defines "HZ_DIST"
+		buildoptions "/utf-8"
 		optimize "ON"
